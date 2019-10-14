6269070a4259967cf3022c90bc0f6ee2920b126c
RyukieSama <ryukie.sama@gmail.com>
2019-10-14 23:56:43 +0800

    <UIView> Frame

--- Swifty/UIKit/UIView/View/UIView+Swifty.swift
+++ Swifty/UIKit/UIView/View/UIView+Swifty.swift
@@ -9,19 +9,35 @@
 import UIKit
 
 public extension Swifty where Base: UIView {
-    func x(x: CGFloat) {
+    var x: CGFloat {
+        return base.frame.origin.x
+    }
+    
+    var y: CGFloat {
+        return base.frame.origin.y
+    }
+    
+    var width: CGFloat {
+        return base.frame.size.width
+    }
+    
+    var height: CGFloat {
+        return base.frame.size.height
+    }
+    
+    func setX(x: CGFloat) {
         base.frame = CGRect(x: x, y: base.frame.origin.y, width: base.frame.size.width, height: base.frame.size.height)
     }
     
-    func y(y: CGFloat) {
+    func setY(y: CGFloat) {
         base.frame = CGRect(x: base.frame.origin.x, y: y, width: base.frame.size.width, height: base.frame.size.height)
     }
     
-    func width(width: CGFloat)  {
+    func setWidth(width: CGFloat)  {
         base.frame = CGRect(x: base.frame.origin.x, y: base.frame.origin.y, width: width, height: base.frame.size.height)
     }
     
-    func height(height: CGFloat) {
+    func setHeight(height: CGFloat) {
         base.frame = CGRect(x: base.frame.origin.x, y: base.frame.origin.y, width: base.frame.size.width, height: height)
     }
 }
