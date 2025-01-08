fa3154bb86186c946a6cba75d7c50b5fca4dbe65
Lab.Lawliet <ryukie.sama@gmail.com>
2025-01-08 11:10:02 +0800

    Log

--- Swifty/Classes/Core/SwiftyLog.swift
+++ Swifty/Classes/Core/SwiftyLog.swift
@@ -9,8 +9,13 @@ import Foundation
 
 public func SwiftyLog(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, lineNumber: Int = #line) {
     #if DEBUG // 判断是否在测试环境下
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    let currentTime = Date()
+    let currentTimeString = dateFormatter.string(from: currentTime)
     let fileName = (file as NSString).lastPathComponent
-    print("[\(fileName) - line:\(lineNumber)]")
+    print("[\(fileName) - line:\(lineNumber) - time:\(currentTimeString)]")
     print(items, separator, terminator)
     #else
     #endif
